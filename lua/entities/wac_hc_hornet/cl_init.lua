d4c0af7aad8b008b3b7a6dcc4fcdd6c12272f02a
Matt Jeanes <mattjeanes23@gmail.com>
2015-03-05 19:16:21 +0000

    Fixed for next gmod update

--- lua/entities/wac_hc_hornet/cl_init.lua
+++ lua/entities/wac_hc_hornet/cl_init.lua
@@ -11,22 +11,25 @@ function ENT:Think()
 	self:base("wac_hc_base").Think(self)
 	local throttle = self:GetNWFloat("up", 0)
 	local active = self:GetNWBool("active", false)
-	local ent=LocalPlayer():GetVehicle():GetNWEntity("wac_aircraft")
-	if self==ent and active and throttle>0 and CurTime()>cureffect then
-		cureffect=CurTime()+0.1
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(5,115,25)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(20)
-		util.Effect("wac_halo_effect", ed)
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(5,-115,25)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(20)
-		util.Effect("wac_halo_effect", ed)
+	local v=LocalPlayer():GetVehicle()
+	if IsValid(v) then
+		local ent=v:GetNWEntity("wac_aircraft")
+		if self==ent and active and throttle>0 and CurTime()>cureffect then
+			cureffect=CurTime()+0.1
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(5,115,25)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(20)
+			util.Effect("wac_halo_effect", ed)
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(5,-115,25)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(20)
+			util.Effect("wac_halo_effect", ed)
+		end
 	end
 end
--- lua/entities/wac_hc_pelican/cl_init.lua
+++ lua/entities/wac_hc_pelican/cl_init.lua
@@ -11,36 +11,39 @@ function ENT:Think()
 	self:base("wac_hc_base").Think(self)
 	local throttle = self:GetNWFloat("up", 0)
 	local active = self:GetNWBool("active", false)
-	local ent=LocalPlayer():GetVehicle():GetNWEntity("wac_aircraft")
-	if self==ent and active and throttle>0 and CurTime()>cureffect then
-		cureffect=CurTime()+0.1
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(57.72,185,78.41)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(15)
-		util.Effect("wac_halo_effect", ed)
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(57.72,-185,78.41)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(15)
-		util.Effect("wac_halo_effect", ed)
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(-409.43,120,200)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(15)
-		util.Effect("wac_halo_effect", ed)
-		
-		local ed=EffectData()
-		ed:SetEntity(self)
-		ed:SetOrigin(Vector(-409.43,-120,200)) // offset
-		ed:SetMagnitude(throttle)
-		ed:SetRadius(15)
-		util.Effect("wac_halo_effect", ed)		
+	local v=LocalPlayer():GetVehicle()
+	if IsValid(v) then
+		local ent=v:GetNWEntity("wac_aircraft")
+		if self==ent and active and throttle>0 and CurTime()>cureffect then
+			cureffect=CurTime()+0.1
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(57.72,185,78.41)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(15)
+			util.Effect("wac_halo_effect", ed)
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(57.72,-185,78.41)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(15)
+			util.Effect("wac_halo_effect", ed)
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(-409.43,120,200)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(15)
+			util.Effect("wac_halo_effect", ed)
+			
+			local ed=EffectData()
+			ed:SetEntity(self)
+			ed:SetOrigin(Vector(-409.43,-120,200)) // offset
+			ed:SetMagnitude(throttle)
+			ed:SetRadius(15)
+			util.Effect("wac_halo_effect", ed)		
+		end
 	end
 end
